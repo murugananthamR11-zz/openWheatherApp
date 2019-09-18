4753191556848da435d359afc4e9fd3097818ff9
Muruganantham R <muruganantham.r@rxp.com>
2019-09-18 15:02:13 +0530

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import{ WheatherService} from '../app/wheather.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'openWheatherApp';
+export class AppComponent implements OnInit {
+
+ searchValue:string;
+ wheatherShow:boolean;
+ WheatherError:boolean;
+ wheatherData;
+ cardsArray =[]
+ emptyField:boolean = false;
+ loading:boolean = false;
+ addShow:boolean =false;
+ removeShow:boolean = false;
+ cardArryIndex:number;
+ date = new Date();
+ constructor(private ws:WheatherService){}
+
+  ngOnInit(){
+    console.log(this.date)
+  }
+
+  onSearch(){
+
+    if(!this.searchValue){
+      this.emptyField = true;
+      return;
+    }
+
+    this.loading = true;
+
+    this.ws.getWheather(this.searchValue).subscribe(res => {
+      this.wheatherData = res;
+      this.wheatherShow = true;
+      this.addShow = true;
+      this.loading = false;
+    },(err) => {
+      this.WheatherError = true;
+      this.loading = false;
+      this.addShow = false;
+    });
+  }
+  addCard(){
+    this.cardsArray.unshift(this.wheatherData);
+    this.wheatherShow = false;
+    this.addShow = false;
+    this.searchValue = '';
+    console.log(this.cardsArray)
+  }
+  popClose(){
+    this.wheatherShow = false;
+    this.WheatherError = false;
+    this.addShow = false;
+    this.removeShow = false;
+    this.emptyField = false;
+    this.searchValue = '';
+  }
+  openCard(i){
+    this.cardArryIndex = i
+    this.wheatherData = this.cardsArray[i];
+    this.wheatherShow = true;
+    this.removeShow = true;
+  }
+  removeCard(){
+    this.wheatherShow = false;
+    this.removeShow = false;
+    console.log(this.cardArryIndex);
+    this.cardsArray.splice(this.cardArryIndex,1)
+  }
 }
---
+++ src/app/wheather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WheatherService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getWheather(searchValue){
+    return this.http.get('https://openweathermap.org/data/2.5/weather?q='+searchValue+'&appid=b6907d289e10d714a6e88b30761fae22')
+  }
+}
